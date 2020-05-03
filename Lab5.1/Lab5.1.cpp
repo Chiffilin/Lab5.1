59053d0beef3672fbe9a6770a5e1dcd60e6c8335
Chiffilin <dimaaitbaev42@gmail.com>
2020-05-03 14:21:06 +0300

    Добавьте файлы проекта.

---
+++ Lab5.1/Lab5.1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int a, b, c, d, e, g,ans;
+	ifstream F;
+	ofstream Test;
+	Test.open("D:\\Games\\Test.txt", ios::out);
+	F.open("D:\\Games\\f.txt", ios::in);
+	F >> a >> b >> c >> d >> e >> g;
+	ans = a * b * c * d * e * g;
+	Test << "Ответ: "<<ans;
+	F.close();
+	Test.close();
+	system("pause");
+	return 0;
+}
